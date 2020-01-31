19284d13f9d852a0945af13fd174fcfd665ac012
TomiSalo <tomi.a.salo@outlook.com>
2020-01-31 20:24:25 +0200

    orderit haettu tietokannasta

---
+++ build/precache-manifest.6ad695c7ccdeb1955611be332118be09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a37e6f654a2f2c7cbcac9599c0ce7cf",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "a6670e458db9a18bf56b",
+    "url": "/burger/static/css/main.fbba1216.chunk.css"
+  },
+  {
+    "revision": "e7961041cb1c44471aea",
+    "url": "/burger/static/js/2.e8819732.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/burger/static/js/2.e8819732.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6670e458db9a18bf56b",
+    "url": "/burger/static/js/main.eb2f0eed.chunk.js"
+  },
+  {
+    "revision": "286f14b575b5e68c3dc6",
+    "url": "/burger/static/js/runtime-main.6731923e.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
