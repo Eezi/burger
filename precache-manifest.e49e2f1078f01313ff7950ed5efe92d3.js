fbba6ae7cefb52127925be7b84b815507449c72e
TomiSalo <tomi.a.salo@outlook.com>
2020-02-09 21:31:40 +0200

    Updates

--- precache-manifest.e49e2f1078f01313ff7950ed5efe92d3.js
+++ precache-manifest.e49e2f1078f01313ff7950ed5efe92d3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9a37e6f654a2f2c7cbcac9599c0ce7cf",
+    "revision": "6e388cf72b6873d3c486eca64ac02487",
     "url": "/burger/index.html"
   },
   {
-    "revision": "a6670e458db9a18bf56b",
-    "url": "/burger/static/css/main.fbba1216.chunk.css"
+    "revision": "cef8cd5fa4f53452c8d2",
+    "url": "/burger/static/css/main.9100ccdc.chunk.css"
   },
   {
-    "revision": "e7961041cb1c44471aea",
-    "url": "/burger/static/js/2.e8819732.chunk.js"
+    "revision": "faf23ffb6c3d0b51c475",
+    "url": "/burger/static/js/2.2fbb3a8f.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/burger/static/js/2.e8819732.chunk.js.LICENSE"
+    "url": "/burger/static/js/2.2fbb3a8f.chunk.js.LICENSE"
   },
   {
-    "revision": "a6670e458db9a18bf56b",
-    "url": "/burger/static/js/main.eb2f0eed.chunk.js"
+    "revision": "cef8cd5fa4f53452c8d2",
+    "url": "/burger/static/js/main.d9d28b01.chunk.js"
   },
   {
     "revision": "286f14b575b5e68c3dc6",
