30d4471b5970adfeca16f40b437f3c427df9c93e
TomiSalo <tomi.a.salo@outlook.com>
2020-02-12 20:29:20 +0200

    tehtiin action creators

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import reducer from './store/reducer';
-
-const store = createStore(reducer);
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
+//Lisätään redux devtools
+const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
     <Provider store={store}>
