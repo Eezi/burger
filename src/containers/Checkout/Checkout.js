7e6546ad5cb336d58f4c5d98745ed5360f36c081
TomiSalo <tomi.a.salo@outlook.com>
2020-02-14 16:52:29 +0200

    Lisätty parannettu redux projektiin

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -10,23 +10,7 @@ class Checkout extends Component {
     componentWilldMount () {
         this.props.onInitPurchase();
     }
-
-    /*componentWillMount () {
-        const query = new URLSearchParams( this.props.location.search );
-        const ingredients = {};
-        let price = 0;
-        for ( let param of query.entries() ) {
-            // ['salad', '1']
-            if (param[0] === 'price') {
-                price = param[1];
-            } else {
-                ingredients[param[0]] = +param[1];
-            }
-        }
-        this.setState( { ingredients: ingredients, totalPrice: price } );
-    }*/
-
-
+    
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
     }
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,6 +1,7 @@
 import * as actionTypes from './actionTypes'
 import axios from '../../axios-orders'
 
+
 export const purchaseBurgerSuccess = (id, orderData) => {
     return {
         type: actionTypes.PURCHASE_BURGER_SUCCESS,
@@ -39,5 +40,45 @@ export const purchaseBurger = (orderData) => {
 export const purchaseInit = () => {
     return {
         type: actionTypes.PURCHASE_INIT
+    }
+}
+
+export const fetchedOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCES,
+        orders: orders
+    }
+}
+
+export const fetchOrdersFail = (error) => {
+    return  {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    }
+}
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    }
+}
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart())
+        axios.get('/orders.json')
+        .then(res => {
+            const fetchedOrders = [];
+            for (let key in res.data) {
+                fetchedOrders.push({
+                    ...res.data[key],
+                    id: key
+                });
+            }
+            dispatch(fetchedOrdersSuccess(fetchedOrders));
+        })
+        .catch(err => {
+            dispatch(fetchedOrdersSuccess(fetchOrders))
+        });
     }
 }
